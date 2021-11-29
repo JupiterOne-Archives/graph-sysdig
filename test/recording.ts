d63c148c75ee14e9374e0c974108a46f245eb2ff
Stefan Virag <viragsf@gmail.com>
2021-11-29 13:48:20 +0100

    Use gzip to modify binary response

--- test/recording.ts
+++ test/recording.ts
@@ -2,14 +2,14 @@ import {
   Recording,
   setupRecording,
   SetupRecordingInput,
+  mutations,
 } from '@jupiterone/integration-sdk-testing';
 
 export { Recording };
 
-export function setupSysdigRecording(
-  input: Omit<SetupRecordingInput, 'mutateEntry'>,
-): Recording {
+export function setupSysdigRecording(input: SetupRecordingInput): Recording {
   return setupRecording({
+    mutateEntry: mutations.unzipGzippedRecordingEntry,
     ...input,
     options: {
       matchRequestsBy: {
