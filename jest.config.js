73cec0b3f48f2ae93a483933a711e0a1eae45573
Max Daum <max.daum@jupiterone.com>
2022-10-26 12:17:24 -0400

    only run one test

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,9 @@
-module.exports = require('@jupiterone/integration-sdk-dev-tools/config/jest');
+module.exports = {
+  ...require('@jupiterone/integration-sdk-dev-tools/config/jest'),
+  testMatch: [
+    '<rootDir>/src/steps/teams/index.test.ts',
+    '!**/node_modules/*',
+    '!**/dist/*',
+    '!**/*.bak/*',
+  ],
+};
